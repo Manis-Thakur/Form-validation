aef71df602a690558e56438932a79eb7a149f241
Manish Thakur <manisthakur2004@gmail.com>
2025-03-11 13:07:42 +0545

    updated email

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ document.getElementById('contactForm').addEventListener('submit', function (even
     event.preventDefault();
     let name = document.getElementById('nameOfUser');
     let Dob = document.getElementById('dateOfUser');
-
+    let email = document.getElementById('emailUser');
 
     document.getElementById('nameError').innerHTML = "";
-    document.getElementById('DobError'.innerHTML = "");
+    document.getElementById('DobError').innerHTML = "";
+    document.getElementById('EmailError').innerHTML = "";
 
     let isValid = true;
 
@@ -17,6 +18,10 @@ document.getElementById('contactForm').addEventListener('submit', function (even
     if (Dob.value.trim() === '') {
         document.getElementById('DobError').innerHTML = "Enter your DOB";
         isValid = false;
+    }
+
+    if(email.value.trim()=== ''){
+        document.getElementById('EmailError').innerHTML = "Enter your email";
     }
 
     if (isValid) {
